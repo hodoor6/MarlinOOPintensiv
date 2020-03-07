c99ad3390e2410d4b97ffad9e54a3f06740cd970
hodoor6 <hodoor6@yandex.ua>
2020-03-07 14:35:04 +0200

    Компонент Database. Метод Query #2 Task 4 реализация метода Query для написание запроса и посилание его в бд и выборка результатов создание свойста Getters public query для присвоения подключение к бд и передачи данных в в бд в метод execute cоздание свойства и Getters private $results для получения результатов выборки из бд и их вывод на вью  cоздание свойства и Getters private $error = false, для для вывода ошибок при выборке и написанни запроса
     cоздание свойства и Getters private $count; для получения количества столбцов при виборке из бд;
     теперь подкючение к бд выполняеться спомошью статического свойства DataBase::getInstance();

--- Components/DataBase.php
+++ Components/DataBase.php
@@ -13,45 +13,47 @@ class DataBase
     private $charset;
     private $pdo;
 
+    // свойства которые относяться только методу к query;
+    private $query, $error = false, $results, $count;
 
 //созданние приватного свойства для того чтобы по умолчанию подключение к бд было null реализация патерна сингелтон
     private static $instance = null;
-
 // cоздание приватного construct  для того чтобы никто не мог получить доступ к нему реализация патерна сингелтон
-    private function __construct()
-    {
-        $this->driver = 'mysql'; // тип базы данных, с которой мы будем работать
-        $this->host = 'localhost';// альтернатива '127.0.0.1' - адрес хоста, в нашем случае локального
-        $this->dbname = 'MarlinOopComponetsDataBase'; // имя базы данных
-        $this->user = 'root'; // имя пользователя для базы данных
-        $this->password = ''; // пароль пользователя
-        $this->charset = 'utf8'; // кодировка по умолчанию
+        private function __construct()
+        {
+            $this->driver = 'mysql'; // тип базы данных, с которой мы будем работать
+            $this->host = 'localhost';// альтернатива '127.0.0.1' - адрес хоста, в нашем случае локального
+            $this->dbname = 'MarlinOopComponetsDataBase'; // имя базы данных
+            $this->dbuser = 'root'; // имя пользователя для базы данных
+            $this->dbpassword = ''; // пароль пользователя
+            $this->charset = 'utf8'; // кодировка по умолчанию
 
-        $dsn = "$this->driver:host=$this->host;dbname=$this->dbname;charset=$this->charset";
+            $dsn = "$this->driver:host=$this->host;dbname=$this->dbname;charset=$this->charset";
 // Setting options
-        $options = [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-        ];
+            $options = [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+            ];
 // Making the connection to the database
-        try {
-            $this->pdo = new PDO($dsn, $this->dbuser, $this->dbpassword, $options);
-        } catch (PDOException $e) {
-            $this->error = $e->getMessage();
+            try {
+              $this->pdo = new PDO($dsn, $this->dbuser, $this->dbpassword, $options);
+            } catch (PDOException $e) {
+                $this->error = $e->getMessage();
+                var_dump($this->error);
+            }
+
         }
 
-    }
-
 // реализация патерна сингелтон через подключение к базе данных
-    public static function getInstance()
+    public  static function getInstance()
 
     {
-        if (!isset(self::$instance)) {
+        if(!isset(self::$instance)){
 
             self::$instance = new Database();
         }
 
-        return self::$instance;
+        return  self::$instance;
     }
 
     //- получить все записи из таблицы
@@ -63,7 +65,6 @@ class DataBase
         $result = $stmt->fetchAll(PDO::FETCH_OBJ);
         return $result;
     }
-
 //- получить одну запись из таблицы по id
 
     public function getOne($table, $id)
@@ -76,7 +77,6 @@ class DataBase
         $result = $stmt->fetch();
         return $result;
     }
-
     public function insert($table, array $data)
     {
         //удаление елемента массива так как он являеться кнопкой
@@ -98,11 +98,12 @@ class DataBase
 //удаляю последный елемент масива
         array_pop($data);
 
-        $keys = '';
-        foreach ($data as $column => $value) {
+            $keys='';
+        foreach ($data as $column=>$value){
 
-            if ($column != 'id') {
-                $keys .= $column . '=:' . $column . ', ';
+            if($column !='id' )
+            {
+                $keys .= $column.'=:'.$column.', ';
 
             }
         }
@@ -118,7 +119,6 @@ class DataBase
         header("Location:  /Components/testing/index.php");
 
     }
-
     //- удалить запись из таблицы по id
     public function delete($table, $id)
 
@@ -131,13 +131,60 @@ class DataBase
         header("Location:  /Components/testing/index.php");
     }
 
+    //- получить все записи из таблицы универсальный метод
+    public function query($sql)
 
+    {
+        $this->error = false;
+        $this->query = $this->pdo->prepare($sql);
+
+// проверка на уход данных в бд
+        if (!$this->query->execute()) {
+            return $this->error = true;
+
+        } else {
+            //выборка данных из бд
+            $this->results = $this->query->fetchAll(PDO::FETCH_OBJ);
+            //полученные поличество колонок из бд
+            $this->count = $this->query->rowCount();
+        }
+
+// передача только создаваемого класа
+        return $this;
+    }
+// getter получает приватное свойство error
+    public function getError()
+    {
+        return $this->error;
+    }
+// getter получает приватное свойство results
+    public function results()
+    {
+        return $this->results;
+    }
+// getter получает приватное свойство count
+    public function count()
+    {
+        return $this->count;
+    }
 }
+
 //тестинг
 
+//тестирование метода query путем выборки всех пользователей
+$userstest = DataBase::getInstance()->query('SELECT * FROM users');
 
+// вывод ошибок  если при выполнении запроса есть ошибка
+if($userstest->getError()){
+    echo 'есть ошибки';
 
-
+}else{
+    echo 'нет ошибок';
+}
+//вывод результата массива что получился при выборке
+$userstest->results();
+// подсчет количества выбранных строк
+var_dump($userstest->count());
 
 
 
